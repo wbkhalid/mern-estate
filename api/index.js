bee8184d48c89989bc940cbc9fa5182f87c9cf92
Waleed Bin Khalid <waleedbinkhalidch@gmail.com>
2024-05-06 09:54:52 +0500

    creating user model

--- api/index.js
+++ api/index.js
@@ -7,10 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTIONSTRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.CONNECTIONSTRING)
   .then(() => {
     console.log("Connected to MongoDB");
   })
