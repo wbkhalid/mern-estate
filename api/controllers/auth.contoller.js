6737a82f475821ed4a211574ff155fa9ee0d4b19
Waleed Bin Khalid <waleedbinkhalidch@gmail.com>
2024-05-10 13:01:34 +0500

    ceate sig in api

--- api/controllers/auth.contoller.js
+++ api/controllers/auth.contoller.js
@@ -1,6 +1,8 @@
 import User from "../model/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utlils/error.js";
+import jwt from "jsonwebtoken";
+
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -11,5 +13,23 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error);
     // next(errorHandler(500, "message handler function"));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    !validUser && next(errorHandler(404, "User not exist"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    !validPassword && next(errorHandler(401, "Incorrect Credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_TOKEN);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
